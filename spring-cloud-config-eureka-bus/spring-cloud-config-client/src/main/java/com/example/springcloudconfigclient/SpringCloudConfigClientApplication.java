5ad254de8236fedd1f7c75b39c69c20d30f0101b
zhuhongbo <1010724827@qq.com>
2023-06-20 22:27:15 +0800

    配置中心+消息总线Demo

--- spring-cloud-config-eureka-bus/spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-eureka-bus/spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -2,12 +2,14 @@ package com.example.springcloudconfigclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudConfigClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+    }
 
 }
--- spring-cloud-config-eureka-bus/spring-cloud-config-server/src/main/java/com/example/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-eureka-bus/spring-cloud-config-server/src/main/java/com/example/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,12 +2,16 @@ package com.example.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    }
 
 }
